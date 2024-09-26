fa1bc3222132e9cfb9669316d1de7dd54e431044
Liza-Khube <elisekhu@gmail.com>
2024-09-26 19:44:26 +0300

    Update Exercise_1.js

--- Lab1/Exercise_1.js
+++ Lab1/Exercise_1.js
@@ -4,18 +4,22 @@
 
 const inc1 = (x) => ++x;
 
-//Test for Exercise 1.1
+/*
+Test for Exercise 1.1
 
 const a = 6;
 const b = inc1(a);
 console.dir({ a, b });
+*/
 
 //Exercise 1.2
 
 const inc2 = (num) => ++num.n;
 
-//Test for Exercise 1.2
+/*
+Test for Exercise 1.2
 
 const obj = { n: 8 };
 inc2(obj);
 console.dir(obj);
+*/
