b8563b8e6c6a49a09ee71f13f73d27ff068a56de
Liza-Khube <elisekhu@gmail.com>
2024-09-26 19:42:47 +0300

    Create Exercise_1.js

---
+++ Lab1/Exercise_1.js
@@ -0,0 +1,21 @@
+'use strict';
+
+//Exercise 1.1
+
+const inc1 = (x) => ++x;
+
+//Test for Exercise 1.1
+
+const a = 6;
+const b = inc1(a);
+console.dir({ a, b });
+
+//Exercise 1.2
+
+const inc2 = (num) => ++num.n;
+
+//Test for Exercise 1.2
+
+const obj = { n: 8 };
+inc2(obj);
+console.dir(obj);
